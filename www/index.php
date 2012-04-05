2ec01a91045850a851afa0151eba491ce8008790
Vincent Bruijn <vebruijn@gmail.com>
2012-04-05 18:46:38 +0200

    added caching

--- www/index.php
+++ www/index.php
@@ -2,20 +2,43 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="description" content="LHOOQ - LHOOGLITCH - GLITCH - ax710 - y_a_v_a">
-	<meta name="keywords" content="">
-	<title>LHOOGLITCH 2 - DUCHAMP</title>
-	<link rel="shortcut icon" href="favicon.ico">
+	<meta name="description" content="LHOOQ - ax710 - y_a_v_a">
+	<meta name="keywords" content="ax710, lhooq, y-a-v-a, marcel duchamp">
+	<title>l-h-o-o-q.org - ax710 - y-a-v-a.org - Duchamp</title>
+	<link rel="shortcut icon" href="favicon.png">
 	<style>
 		body { padding: 0; margin: 0; text-align: center;}
+		img { border: 0; }
+		.color2 { color:rgb(0,174,239); }
+		.color1 { color:rgb(236,0,140); }
+		.color3 { color:rgb(255,242,0); }
 	</style>
 	<script type="text/javascript">
 	myReload = function() {
-		//window.location.reload();
+		window.location.reload();
 	}
 	</script>
 </head>
-<body onload="setTimeout('myReload()', 5000)">
-	<img src="lhooq.php" alt="" title="">
+<body onload="setTimeout('myReload()', 30000)">
+	<img src="lhooq.php" alt="" title=""/>
+	<div>
+		<a class="color2" href="http://l-h-o-o-q.org">l-h-o-o-q.org</a>
+		 van <a class="color1" href="http://www.ax710.org/" rel="cc:attributionURL">ax710</a> &amp; <a class="color3" href="http://www.y-a-v-a.org/" rel="cc:attributionURL">y-a-v-a.org</a>
+				is in licentie gegeven volgens een <a class="color2" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.<br><br>
+		<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/"><img alt="Creative Commons License" src="http://creativecommons.org/images/public/somerights20.png" /></a>
+	</div>
+	
+	
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-16179947-8']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
 </body>
 </html>
--- www/lhooq.php
+++ www/lhooq.php
@@ -1,10 +1,19 @@
 <?php
 error_reporting(E_ALL);
 // instantiate new Mona class and echo the image
-require_once '../share/Mona.class.php';
+require_once 'lib/Mona.class.php';
 $img = new Mona();
+$image = null;
 
 header('Content-type: image/jpeg');
-echo $img->build()->show();
+if (rand(0,2) === 0) {
+	$data = $img->build()->show();
+	$image = "cache/". uniqid("img-") . '.jpg';
+	@file_put_contents($image ,$data);
+} else {
+	$imgs = glob('cache/*.jpg');
+	$image = $imgs[array_rand($imgs)];
+}
+echo @file_get_contents($image);
 
 exit();
